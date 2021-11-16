0fad50d1e65b16b9febc15cede56c2011dd39aff
ytrop <62332814+ytrop@users.noreply.github.com>
2021-11-16 10:46:05 +0100

    Add files via upload

---
+++ LogPruebas/src/test/java/com/nttdata/bootcamp/logpruebas/LogPruebasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.logpruebas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogPruebasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
